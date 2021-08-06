d352825789ca644cf57a1a660a0f43fd39f5d61e
fuwamocha <74049905+fuwamocha@users.noreply.github.com>
2021-08-06 13:50:51 +0900

    Assetsフォルダ追加

---
+++ Assets/Scripts/CameraWork.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour
+{
+    GameObject playerObj;
+    Transform playerTransform;
+    void Start()
+    {
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = playerObj.transform;
+    }
+    void LateUpdate()
+    {
+        MoveCamera();
+    }
+    void MoveCamera()
+    {
+        //横方向だけ追従
+        transform.position = new Vector3(transform.position.x, playerTransform.position.y, transform.position.z);
+    }
+}
